b1cf3480234643447b3cd66ed4b4cd1ab8e4e3f5
Christian Carter <christian@biggerboatconsulting.com>
2016-05-20 23:58:35 -0700

    build pair of counters app

---
+++ client/source/Counter.elm
@@ -0,0 +1,67 @@
+module Counter exposing (Model, Msg, init, update, view)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Ui.Container
+import Ui.Button
+
+
+-- MODEL
+
+
+type alias Model =
+    { container : Ui.Container.Model
+    , counter : Int
+    }
+
+
+init : Int -> Model
+init count =
+    { counter = 0
+    , container =
+        { direction = "ltr"
+        , align = ""
+        , compact = False
+        }
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            {model | counter = model.counter + 1}
+
+        Decrement ->
+            {model | counter = model.counter - 1}
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    Ui.Container.view model.container
+        []
+        [ Ui.Container.column []
+            [ div []
+                [ span [] [ text "Counter:" ]
+                , strong [] [ text (toString model.counter) ]
+                ]
+            , Ui.Container.row []
+                [ Ui.Button.primary "Decrement" Decrement
+                , Ui.Button.primary "Increment" Increment
+                ]
+            ]
+        ]
--- client/source/Main.elm
+++ client/source/Main.elm
@@ -1,77 +1,80 @@
 module Main exposing (..)
 
 import Html exposing (div, span, strong, text)
+import Html.Attributes exposing (href)
 import Html.App
-
 import Ui.Container
 import Ui.Button
+import Ui.IconButton
 import Ui.App
 import Ui
+import Counter
 
 
 type alias Model =
-  { app : Ui.App.Model
-  , counter : Int
-  }
+    { app : Ui.App.Model
+    , topCounter : Counter.Model
+    , bottomCounter : Counter.Model
+    }
 
 
 type Msg
-  = App Ui.App.Msg
-  | Increment
-  | Decrement
+    = App Ui.App.Msg
+    | Top Counter.Msg
+    | Bottom Counter.Msg
+    | Reset
 
 
-init : Model
-init =
-  { app = Ui.App.init "Elm-UI Project"
-  , counter = 0
-  }
+init : Int -> Int -> Model
+init top bottom =
+    { app = Ui.App.init "SOBS"
+    , topCounter = Counter.init top
+    , bottomCounter = Counter.init bottom
+    }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    App act ->
-      let
-        ( app, effect ) =
-          Ui.App.update act model.app
-      in
-        ( { model | app = app }, Cmd.map App effect )
+    case msg of
+        App act ->
+            let
+                ( app, effect ) =
+                    Ui.App.update act model.app
+            in
+                ( { model | app = app }, Cmd.map App effect )
 
-    Increment ->
-      ( { model | counter = model.counter + 1 }, Cmd.none )
+        Top msg ->
+            ( { model | topCounter = Counter.update msg model.topCounter }, Cmd.none )
 
-    Decrement ->
-      ( { model | counter = model.counter - 1 }, Cmd.none )
+        Bottom msg ->
+            ( { model | bottomCounter = Counter.update msg model.bottomCounter }, Cmd.none )
+
+        Reset ->
+            ( { model | topCounter = Counter.init 0, bottomCounter = Counter.init 0 }, Cmd.none )
 
 
 view : Model -> Html.Html Msg
 view model =
-  Ui.App.view
-    App
-    model.app
-    [ Ui.Container.column
-        []
-        [ Ui.title [] [ text "Elm-UI Counter" ]
-        , Ui.textBlock "This is an minimal project to get you started with Elm-UI!"
-        , div
-            []
-            [ span [] [ text "Counter:" ]
-            , strong [] [ text (toString model.counter) ]
+    Ui.App.view App
+        model.app
+        [ Ui.Container.column []
+            [ Ui.title [] [ text "S3 Elm UI" ]
+            , Ui.textBlock "This is the basic Elm counter demo. I have turned the counter into its own module, and included it twice. Components! I also am using the elm-ui library as a styling framework. The page is automatically compiled and pushed to amazon s3 by a codeship deploy and the serverless project. "
+            , Html.a [ href "http://github.com/cdcarter/sobs" ] [ text "view on github" ]
             ]
-        , Ui.Container.row
-            []
-            [ Ui.Button.primary "Decrement" Decrement
-            , Ui.Button.primary "Increment" Increment
+        , Ui.Container.row []
+            [ Html.App.map Top (Counter.view model.topCounter)
+            , Html.App.map Bottom (Counter.view model.bottomCounter)
             ]
+        , Ui.Container.row []
+            [ Ui.IconButton.primary "Reset" "ion-ios-refresh-outline" "left" Reset ]
         ]
-    ]
 
 
 main =
-  Html.App.program
-    { init = ( init, Cmd.none )
-    , view = view
-    , update = update
-    , subscriptions = \_ -> Sub.none
-    }
+    Html.App.program
+        { init = ( init 0 0, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
